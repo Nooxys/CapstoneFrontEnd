1e5c0ed14b29ec9f9490213c7f32b9a2c32ba18b
Ciro Vitiello <ciro.riccione@gmail.com>
2024-05-09 12:25:39 +0200

    styles and page not found

---
+++ src/components/AboutUS.jsx
@@ -0,0 +1,12 @@
+import { Container, Row, Col } from 'react-bootstrap'
+
+const AboutUs = () => {
+  return (
+    <Container>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
+  )
+}
+export default AboutUs
